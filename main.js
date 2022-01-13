e782b1401d322582671c74850ae6268f631db34b
MedhaManasvini <79033962+MedhaManasvini@users.noreply.github.com>
2022-01-13 10:37:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,3 +23,26 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded(){
     console.log("model_loaded")
 }
+function identify(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.log(error);
+    } else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+     document.getElementById("object_accuracy").innerHTML =      parseInt(results[0].confidence.toFixed(3));
+        console.log(typeof(results[0].confidence.toFixed(3)));
+   
+    }
+}
+
+//value_of_accuracy =Number(document.getElementById("object_accuracy").value);
+
+
+//percent = (value_of_accuracy/100)+"%";
+
+//document.getElementById("object_accuracy").innerHTML=percent;
